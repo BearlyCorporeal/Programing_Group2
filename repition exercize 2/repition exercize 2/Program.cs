d74e2f7692a618780744200f409747938cd8665c
BearlyCorporeal <48078310+BearlyCorporeal@users.noreply.github.com>
2019-03-21 17:22:40 +1100

    completed exxercize 2 part 1

---
+++ repition exercize 2/repition exercize 2/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 102570136
+ * Date: 21/03/2019
+ * Time: 4:10 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace repition_exercize_2 {
+	class Program {
+		public static void Main(string[] args) {
+			int counter = 1;
+			int counter2 = 0;
+			while (counter < 5) {
+				while (counter2 > 0) {
+					Console.Write("X");
+					counter = counter - 1;
+					if (counter == 0) {
+						counter2 = counter2 + 1;
+						counter = 1 + counter2;
+						Console.WriteLine();
+					}
+				}
+			}
+			// TODO: Implement Functionality Here
+
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
